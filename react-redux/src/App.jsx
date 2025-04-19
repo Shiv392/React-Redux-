7fc6e985eef75571316170e639b0f1a885aba4f0
Shiv Soni <sonishiv309@gmail.com>
2025-04-19 13:06:22 +0530

    added router and lazy loading

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import Counter from './components/Counter/Counter';
-import TodoContainer from './components/Todoapp';
+import { lazy } from 'react';
+
+const Counter=lazy(()=> import('./components/Counter/Counter'));
+const Todoapp=lazy(()=> import('./components/Todoapp/index'));
+const Home = lazy(()=> import('./components/index'));
 
 function App() {
-
-
   return (
-    <Provider store={store}>
-      <>
-     <Counter />
-     <TodoContainer />
-      </>
-    </Provider>
+   <Provider store={store}>
+   <BrowserRouter>
+   <Routes>
+   <Route path='/' element={<Home />}>
+   <Route path='/' element={<Counter />}></Route>
+   <Route path ='/todoapp' element={<Todoapp />}></Route>
+   </Route>
+   </Routes>
+   </BrowserRouter>
+   </Provider>
   )
 }
 
